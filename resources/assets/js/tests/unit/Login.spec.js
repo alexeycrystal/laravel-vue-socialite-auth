2ced3d6e99cd47867e0a526236ead91935cb9729
alexeycrystal <alex.shabramov@gmail.com>
2017-10-27 15:00:17 +0300

    Update - Karma config (part 3)

--- resources/assets/js/tests/unit/Login.spec.js
+++ resources/assets/js/tests/unit/Login.spec.js
@@ -1,9 +1,10 @@
-import Editor from '../../views/Login.vue';
+import Vue from 'vue'
+import Editor from '../../views/Auth/Login.vue';
 
 describe('Editor', () => {
     it('should set correct default data', function () {
-        expect(typeof Editor.data).toBe('function')
-        var defaultData = Editor.data()
-        expect(defaultData.input).toBe('# Hello!')
+        expect(typeof Editor.data).toBe('function');
+        var defaultData = Editor.data();
+        expect(defaultData.input).toBe('# Hello!');
     });
 });
