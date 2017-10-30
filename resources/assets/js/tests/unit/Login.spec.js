bd37e949d5d8154c4515c8c3fdab8e2ebd84d0ff
alexeycrystal <alex.shabramov@gmail.com>
2017-10-30 10:54:00 +0200

    Update - Karma - Vue login test

--- resources/assets/js/tests/unit/Login.spec.js
+++ resources/assets/js/tests/unit/Login.spec.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
-import Editor from '../../views/Auth/Login.vue';
+import Login from '../../views/Auth/Login.vue';
 
-describe('Editor', () => {
-    it('should set correct default data', function () {
-        expect(typeof Editor.data).toBe('function');
-        var defaultData = Editor.data();
-        expect(defaultData.input).toBe('# Hello!');
+describe("Login.vue component", function() {
+    it('Component has a created action', () => {
+        expect(typeof Login.created).toBe('function');
+    });
+
+    it('Component should have data', function () {
+        expect(typeof Login.data).toBe('function');
+    });
+
+    it('Component sets the correct default data', () => {
+        const defaultData = Login.data();
+        expect(defaultData.form).toEqual({email:'', password:''});
+        expect(defaultData.isProcessing).toBeFalsy();
+        expect(defaultData.error).toEqual({});
+    });
+
+    it('Component should contain login method', function () {
+        expect(typeof Login.methods.login).toBe('function');
+    });
+
+    it('Component should contain socialLogin method', function () {
+        expect(typeof Login.methods.socialLogin).toBe('function');
     });
 });
